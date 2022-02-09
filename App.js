4045227a6612e37f8d8e079d034b317894235928
Tuan Lam <tul57576@temple.edu>
2022-02-09 17:50:32 -0500

    added report button for OtherProfilePage and rating

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
 import Navigation from './src/navigation';
 import ProfilePage from './src/screens/ProfileScreen';
+import OtherProfilePage from './src/screens/OtherProfileScreen';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
       {/* Navigation will go here */}
       {/*<Navigation />*/}
-      <ProfilePage/>
+      {/*<ProfilePage/>*/}
+      <OtherProfilePage/>
     </SafeAreaView>
   );
 }
--- src/screens/OtherProfileScreen/index.js
+++ src/screens/OtherProfileScreen/index.js
@@ -2,19 +2,39 @@ import React from 'react';
 import { Text, SafeAreaView, Image, View, TouchableOpacity } from 'react-native';
 import ProfileScreenButton from '../../components/ProfileScreenButton';
 import styles from '../ProfileScreen/styles';
+import {Rating} from 'react-native-rating-element';
 
 const OtherProfilePage = () => {
     return(
         <SafeAreaView style={styles.container}>
             <Text>Hi from OtherProfilePage</Text>
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={console.log("word")}>
+                <Image 
+                    source={require('../../../assets/flag.jpg')} 
+                    style={[{width:50, height:50, borderRadius:50/2,}, styles.topRightPosition]}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={console.log}>
                 <Image source={require('../../../assets/blank_pfp.png')}
                 style={ {width:100, height:100, borderRadius:100/2}}></Image>
             </TouchableOpacity>
+            
+            <View style={styles.space}/>
+            <Text style={styles.profileNameText}> Jonny Test</Text>
+            <View style={styles.space}/>
+            <Rating
+                rated={4}
+                totalCount={5}
+                size={20}
+                ratingColor={'#99182e'}
+            />
             <View style={styles.space}/>
             <ProfileScreenButton onPress={console.log} text="Message Seller"/>
+            <View style={styles.space}/>
             <ProfileScreenButton onPress={console.log} text="Reviews and Ratings"/>
         </SafeAreaView>
     );
 }
+
+export default OtherProfilePage;
