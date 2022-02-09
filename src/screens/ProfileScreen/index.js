18cda0c183db4fcbf185099808c873a298a2c78a
Tuan Lam <tul57576@temple.edu>
2022-02-09 13:48:04 -0500

    the pfp icon is now button so int he future users can change their pfp

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, Image, Pressable, View } from 'react-native';
+import { Text, SafeAreaView, Image, View, TouchableOpacity } from 'react-native';
 import ProfileScreenButton from '../../components/ProfileScreenButton';
 import styles from './styles'
 
@@ -8,8 +8,11 @@ const ProfilePage = () => {
     return (
         <SafeAreaView style={styles.container}>
         <Text>hi from ProfilePage</Text>
+        <TouchableOpacity onPress={console.log}>
         <Image source={require('../../../assets/blank_pfp.png')} 
             style={ {width:100, height:100, borderRadius:100/2}}/>
+        </TouchableOpacity>
+
         <View style={styles.space}/>
         <ProfileScreenButton onPress={console.log} text="active listing"/>
         <View style={styles.space}/>
