e39a71cb15270f5991ee42eb9178e82b2693a7d2
Tuan Lam <tuanlam15@gmaill.com>
2022-02-09 09:06:34 -0500

    changed pfp image to a circle

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -8,7 +8,9 @@ const ProfilePage = () => {
     return (
         <SafeAreaView style={styles.container}>
         <Text>hi from ProfilePage</Text>
-        <Image source={require('../../../assets/blank_pfp.png')}/>
+        <Image source={require('../../../assets/blank_pfp.png')} 
+            style={ {width:100, height:100, borderRadius:100/2}}/>
+        <View style={styles.space}/>
         <ProfileScreenButton onPress={console.log} text="active listing"/>
         <View style={styles.space}/>
         <ProfileScreenButton onPress={console.log} text="Reviews and Ratings"/>
@@ -16,7 +18,7 @@ const ProfilePage = () => {
         <ProfileScreenButton onPress={console.log} text="Purchased Items"/>
         <View style={styles.space}/>
       
-    <ProfileScreenButton onPress={console.log} text="Sold Items"/>
+    <   ProfileScreenButton onPress={console.log} text="Sold Items"/>
     </SafeAreaView>
     );
 }
