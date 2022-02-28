f23869d4df6c69f3ce842a9acc81b740503e7739
Tuan Lam <tuanlam15@gmaill.com>
2022-02-28 12:37:56 -0500

    added navigation to other screens

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3unifleastorage54a4f075": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,52 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import { StyleSheet, Text, SafeAreaView, Image, Pressable,TouchableOpacity, View } from 'react-native';
-import { Colors } from '../../styles/Colors';
+import { Text, SafeAreaView, Image, Pressable,TouchableOpacity, View } from 'react-native';
 import ProfileScreenButton from '../../components/ProfileScreenButton';
 import styles from './styles'
 import { Rating } from 'react-native-rating-element';
-
+import { useNavigation } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
 
 const ProfilePage = () => {
+    const navigation = useNavigation();
+    
+    const [image, setImage] = useState(null);
+    
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
 
     return (
-        <SafeAreaView style={styles.container}>
+    
+            <SafeAreaView style={styles.container}>
+            
+            <Text>hi from ProfilePage</Text>
+
+            <TouchableOpacity styles = {[styles.topRightPosition]} onPress={console.log}>
+                <Image source={require('../../../assets/cogwheel.jpg')} 
+                style={[{width:50, height:50, borderRadius:50/2,}, styles.topRightPosition]}/>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={pickImage} style={styles.profileButton}>
+            <Image source={{uri: image}} 
+                style={ {width:100, height:100, borderRadius:100/2}}/>
+            </TouchableOpacity>
         
-        <Text>hi from ProfilePage</Text>
+            <Text style={styles.profileNameText}>John Doe</Text>
 
-        {/* asset not found error */}
-        {/* <Image source={require('../../assets/blank_pfp.png')}/> */}
+            <Rating
+                rated={3.0}
+                totalCount={5}
+                size={20}
+                ratingColor={'#99182e'}
+            />
 
-        
-        {/*the cogwheel button is sus, need to test with actual onPress behavior to see if it works*/}
-        <Pressable onPress={console.log("word")}>
-            <Image source={require('../../../assets/cogwheel.jpg')} 
-            style={[{width:50, height:50, borderRadius:50/2,}, styles.topRightPosition]}/>
-        </Pressable>
+            <View style={styles.space}/>
+                <ProfileScreenButton onPress={() => navigation.navigate("SaveItemScreen")} text="active listing"/>
+                <View style={styles.space}/>
+                <ProfileScreenButton onPress={console.log} text="Reviews and Ratings"/>
+                <View style={styles.space}/>
+                <ProfileScreenButton onPress={ () => navigation.navigate("ForgotPasswordScreen")} text="Change Password"/>
+            </SafeAreaView>
 
-        <TouchableOpacity onPress={console.log}>
-        <Image source={require('../../../assets/blank_pfp.png')} 
-            style={ {width:100, height:100, borderRadius:100/2}}/>
-        </TouchableOpacity>
-        
-        <Text style={styles.profileNameText}>John Doe</Text>
-
-        <Rating
-            rated={3.0}
-            totalCount={5}
-            size={20}
-            ratingColor={'#99182e'}
-        />
-
-        <View style={styles.space}/>
-        <ProfileScreenButton onPress={console.log} text="active listing"/>
-        <View style={styles.space}/>
-        <ProfileScreenButton onPress={console.log} text="Reviews and Ratings"/>
-        <View style={styles.space}/>
-        <ProfileScreenButton onPress={console.log} text="Purchased Items"/>
-        <View style={styles.space}/>
-        <ProfileScreenButton onPress={console.log} text="Sold Items"/>
-    </SafeAreaView>
     );
 }
 
