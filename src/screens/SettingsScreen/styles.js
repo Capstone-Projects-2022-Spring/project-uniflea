8f759ec7a5a92ca8664060be26d3839a8ddf9f57
Kasie Madden <Kasie.madden@gmail.com>
2022-03-16 02:08:23 -0400

    added style for icons

--- src/screens/SettingsScreen/styles.js
+++ src/screens/SettingsScreen/styles.js
@@ -1,3 +1,4 @@
+import { readDirectoryAsync } from "expo-file-system";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
@@ -5,23 +6,53 @@ const styles = StyleSheet.create({
     // flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
-    padding: 10
-  },
-  pageContainer: {
     padding: 10,
+  },
+  pageContainerIcon: {
+    padding: 10,
+  },
+  icon: {
+    paddingRight:28,
+
+  },
+  iconStyle: {
+    flexDirection: 'row',
+    padding:1,
+
+    justifyContent:'space-around',
+    alignItems:'center',
+  },
+
+  inputStyle: {
+    flex: 1,
+    padding:20,
+
   },
   settingsText: {
     fontSize: 24,
     marginBottom: 10,
     color: '#403d3d',
-    fontFamily: 'Arial',  
+    fontFamily: 'Arial',
+ 
+  },
+  containerText: {
+    fontSize: 14,
+    height:50,
+    width: '100%',
+    paddingTop:18,
+    paddingLeft:10,
+    color: 'gray',
+    fontFamily: 'Arial', 
+    borderBottomWidth:1,
+    borderColor:'gray',
   },
   inputContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent:'flex-end',
+    alignItems:'center',
   }, 
   buttonContainer: {
     marginVertical: 10,
+    alignItems: 'center',
 },
 
 });
