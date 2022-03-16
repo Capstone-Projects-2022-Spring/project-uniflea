dbeaaa9195b50112ae75235720c3e06e6ade6a53
Dylan Dunda <tui29835@temple.edu>
2022-03-16 14:24:46 -0400

    Added a function with switch to detect which filter is selected

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -25,7 +25,35 @@ const HomeScreen = ({ searchValue }) => {
   const toggleModalOpen = () => {
     setModalOpen(!modalOpen);
   }
-  const [selectedValue, setSelectedValue] = useState("Category 1");
+  const [selectedValue, setSelectedValue] = useState("None");
+
+  const filterSort = (products, itemValue) => {
+      setSelectedValue(itemValue);
+      console.log("Changed sort setting");
+      console.log(itemValue);
+
+      switch(itemValue) {
+      case 'none':
+        console.log("Didn't sort");
+        //No need to sort
+        return;
+
+      case 'category':
+        console.log("Sorted by category");
+        //Sort by category  
+        return;
+
+
+      case 'price':
+        console.log("Sorted by price");
+        //sort by price
+        return;
+
+      default:
+        console.log("Default switch case detected, not sorting.")
+        return;
+    }
+  }
 
   useEffect(() => {
     // query the products in the product list on rendering the page
@@ -38,7 +66,7 @@ const HomeScreen = ({ searchValue }) => {
     {/*Modal used to display filter tab*/}
     <Modal visible={modalOpen} animationType='slide'>
       <View style={styles.modalContent}>
-        <View style={{alignItems: 'flex-end'}}>
+        <View style={{alignItems: 'flex-end', height: 40}}>
           <Feather
             name='check'
             size={40}
@@ -53,13 +81,11 @@ const HomeScreen = ({ searchValue }) => {
             style = {{width: '100%', borderTopWidth: 0}}
             itemStyle={{fontSize:20}}
             selectedValue={selectedValue}
-            onValueChange={(itemValue, itemIndex)=> setSelectedValue(itemValue)}
+            onValueChange={(itemValue, itemIndex)=> filterSort(products, itemValue)}
             >
-              <Picker.Item label ="Category 1" value="category1"/>
-              <Picker.Item label ="Category 2" value="category2"/>
-              <Picker.Item label ="Category 3" value="category3"/>
-              <Picker.Item label ="Category 4" value="category4"/>
-              <Picker.Item label ="Category 5" value="category5"/>
+              <Picker.Item label ="None" value="none"/>
+              <Picker.Item label ="Category" value="category"/>
+              <Picker.Item label ="Price" value="price"/>
 
           </Picker>
         </View>
