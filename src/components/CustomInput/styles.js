27cce109ff4df654150b1e3f726c608b02e56c49
Kasie Madden <Kasie.madden@gmail.com>
2022-03-16 03:28:58 -0400

    adjusted custom input to be a little smaller

--- src/components/CustomInput/styles.js
+++ src/components/CustomInput/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     inputField: {
         height: 50,
-        width: '100%',
+        width: '95%',
         padding: 10,
         margin: 20,
         borderBottomWidth:1,
