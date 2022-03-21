b2626347d7a684659168f82cb6afe89188aaaf83
Kasie Madden <Kasie.madden@gmail.com>
2022-03-21 11:20:36 -0400

    Styling for the reset password page

--- src/screens/ResetPasswordScreen/styles.js
+++ src/screens/ResetPasswordScreen/styles.js
@@ -2,15 +2,31 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     root: {
-        flex: 1,
-        justifyContent: 'center',
-        padding: 10,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+        paddingTop:60,
+        paddingBottom:90,
+        
     },
-    code: {
-        padding: 10,
-    },
+    pageContainer: {
+        padding: 20,
+        paddingBottom: "25%",
+      
+      },
+      space: {
+        width: 20,
+        height: 20,
+      },
+      iconContainer:{
+        marginVertical: 10,
+        alignItems: "center",
+
+      },
     buttonContainer: {
         marginVertical: 10,
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+
     },
     inputContainer: {
         justifyContent: 'center',
@@ -22,14 +38,17 @@ const styles = StyleSheet.create({
     resetHeader: {
         fontSize: 28,
         marginBottom: 30,
-        color: '#403d3d',
-        fontFamily: 'Arial',  
+        color: '#474747',
+        fontFamily: 'Arial',
+        textAlign:'center', 
+
     },
     resetText: {
-        fontSize: 16,
+        fontSize: 12,
         marginBottom: 10,
-        color: '#403d3d',
-        fontFamily: 'Arial',  
+        color: '#474747',
+        fontFamily: 'Arial', 
+         
     },
 
 });
