1f5ab44e5d677c95257877d6c6f6c3cbde70aab1
Kasie Madden <Kasie.madden@gmail.com>
2022-03-21 16:49:39 -0400

    Styling for forget and reset pdw

--- src/screens/ForgotPasswordScreen/styles.js
+++ src/screens/ForgotPasswordScreen/styles.js
@@ -1,41 +1,64 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    root: {
-        flex: 1,
-        justifyContent: 'center',
-        padding: 10,
-    },
-    code: {
-        padding: 10,
-    },
-    buttonContainer: {
-        marginVertical: 10,
-    },
-    inputContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    forgotHeader: {
-        fontSize: 28,
-        marginBottom: 30,
-        color: '#403d3d',
-        fontFamily: 'Arial',  
-    },
-    forgotText: {
-        fontSize: 16,
-        marginBottom: 10,
-        color: '#403d3d',
-        fontFamily: 'Arial', 
-        justifyContent: 'center', 
-    },
-    textContainer: {
-        padding: 10,
-        height: 150,
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-    },
+  root: {
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    paddingTop: 60,
+    paddingBottom: 180,
 
+  
+  },
+
+  pageContainer: {
+    padding: 20,
+    paddingBottom: "25%",
+
+  
+  },
+  space: {
+    width: 20,
+    height: 20,
+  },
+  iconContainer: {
+    marginVertical: 10,
+    alignItems: "center",
+
+  },
+
+  buttonContainer: {
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  forgotHeader: {
+    fontSize: 28,
+    marginBottom: 30,
+    color: "#474747",
+    fontFamily: "Arial",
+    textAlign: "center",
+  },
+  forgotText: {
+    fontSize: 16,
+    marginBottom: 10,
+    color: "#474747",
+    fontFamily: "Arial",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  textContainer: {
+    fontSize: 12,
+    marginBottom: 10,
+    color: "#474747",
+    fontFamily: "Arial",
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    alignItems: "center",
+  },
 });
 
 export default styles;
--- src/screens/ResetPasswordScreen/styles.js
+++ src/screens/ResetPasswordScreen/styles.js
@@ -1,56 +1,49 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    root: {
-        backgroundColor: "#fff",
-        justifyContent: "center",
-        paddingTop:60,
-        paddingBottom:90,
-        
-    },
-    pageContainer: {
-        padding: 20,
-        paddingBottom: "25%",
-      
-      },
-      space: {
-        width: 20,
-        height: 20,
-      },
-      iconContainer:{
-        marginVertical: 10,
-        alignItems: "center",
+  root: {
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    paddingTop: 60,
+    paddingBottom: 90,
+  },
+  pageContainer: {
+    padding: 20,
+    paddingBottom: "25%",
 
-      },
-    buttonContainer: {
-        marginVertical: 10,
+  },
+  space: {
+    width: 20,
+    height: 20,
+  },
+  iconContainer: {
+    marginVertical: 10,
     alignItems: "center",
-    backgroundColor: "#FFFFFF",
-
-    },
-    inputContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    passwordInfo: {
-        color: 'red',
-    },
-    resetHeader: {
-        fontSize: 28,
-        marginBottom: 30,
-        color: '#474747',
-        fontFamily: 'Arial',
-        textAlign:'center', 
-
-    },
-    resetText: {
-        fontSize: 12,
-        marginBottom: 10,
-        color: '#474747',
-        fontFamily: 'Arial', 
-         
-    },
-
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  passwordInfo: {
+    color: "red",
+  },
+  resetHeader: {
+    fontSize: 28,
+    marginBottom: 30,
+    color: "#474747",
+    fontFamily: "Arial",
+    textAlign: "center",
+  },
+  resetText: {
+    fontSize: 12,
+    marginBottom: 10,
+    color: "#474747",
+    fontFamily: "Arial",
+  },
 });
 
 export default styles;
