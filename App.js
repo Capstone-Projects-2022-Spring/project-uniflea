b88bb43d4b9cd254f006b54867f2065014c9fa5c
Kasie Madden <Kasie.madden@gmail.com>
2022-03-22 23:18:20 -0400

    clear pervious local stored data

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StreamChat } from 'stream-chat';
 import { OverlayProvider, Chat } from 'stream-chat-expo';
 
 import Navigation from './src/navigation';
-import Amplify from 'aws-amplify'
+import Amplify,{ Auth, DataStore} from 'aws-amplify';
 import awsconfig from './src/aws-exports'
 
 import AuthContext from './src/contexts/Authentication';
@@ -17,7 +17,14 @@ const client = StreamChat.getInstance(API_KEY);
 
 export default function App() {
   const [user, setUser] = useState('');
-  useEffect(() => {
+
+  // To clear pervious local stored data - Maybe. 
+  const restDataStore = async() => {await  DataStore.clear(), await  DataStore.start()}
+  
+    useEffect(() => {
+    restDataStore();
+
+
     /*
       Amplify provides a unique user identifier (userSub),
       which can be passed as userToken to create a link between the user in the stream database
