2fb234815129ed9fd9aeffd3f094e6de0bd586bd
Kasie Madden <Kasie.madden@gmail.com>
2022-03-22 23:22:56 -0400

    Card list object style changes

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -1,29 +1,69 @@
-
-import { Text, View, Image, Pressable } from 'react-native';
-
-import { useNavigation } from '@react-navigation/native';
-import styles from './styles';
+import { Text, View, Image, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Auth, DataStore } from "aws-amplify";
+import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
+import { Ionicons, AntDesign, FontAwesome } from "@expo/vector-icons";
 
 const ProductItem = ({ id, image, title, price }) => {
-    const navigation = useNavigation();
-    const onPress = () => {
-        // which exact product, passing params lets us send data, but we must also receive data in product details screen for proper function
-        navigation.navigate('ProductScreen', {id: id})
-    }   
-    return (
-        <Pressable onPress={onPress} style={styles.root}>
+  const navigation = useNavigation();
+  const onPress = () => {
+    // which exact product, passing params lets us send data, but we must also receive data in product details screen for proper function
+    navigation.navigate("ProductScreen", { id: id });
+  };
 
-            <Image style={styles.image} source={{ uri: image }} />
-            <View style={styles.rightContainer}>
-                <Text style={styles.title} numberOfLines={3}>{title}</Text>
-    
-                <Text style={styles.price}>${price.toFixed(2)}</Text>
-            </View>
-       
-        </Pressable>
+  const [displayName, setDisplayName] = useState(null);
+ 
+  const seller = async () => {
+    const myUser = await Auth.currentAuthenticatedUser();
+    //i want the single user in the DB with the correct userSUB
+    const userRecord = await DataStore.query(User, (s) =>
+      s.userSub("eq", myUser.attributes.sub)
     );
-}
+    //setting all the data for the users
+    const displayName = userRecord[0].displayName;
+   
+    setDisplayName(displayName);
+  
+  };
 
+  
+  useEffect(() => {
+    seller();
+  }, []);
 
+  return (
+    <Pressable onPress={onPress} style={styles.root}>
+      <Image style={styles.image} source={{ uri: image }} />
+
+      <View style={styles.rightContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title} numberOfLines={3}>
+            {title}
+          </Text>
+        </View>
+
+        <View style={styles.userContainer}>
+          <Text>
+            <AntDesign name="user" size={18} color="black" />{" "}
+            {displayName}
+          </Text>
+        </View>
+        
+        <View style={styles.timeContainer}>
+        <Text style={styles.time}>NEW<FontAwesome name="exclamation" size={17} color="green" /></Text>
+        </View>
+        <View style={styles.priceContainer}>
+          
+            <Text style={styles.price}>
+              <Ionicons name="ios-pricetags-outline" size={18} color="black" />{" "}
+              ${price.toFixed(2)}
+            </Text>
+          </View>
+        </View>
+      
+    </Pressable>
+  );
+};
 
 export default ProductItem;
--- src/components/ProductItem/styles.js
+++ src/components/ProductItem/styles.js
@@ -1,33 +1,69 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
+import Position from "react-native/Libraries/Components/Touchable/Position";
+import { backgroundColor, color } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 const styles = StyleSheet.create({
+  root: {
+    flexDirection: "row",
+    borderColor: "#d1d1d1",
+    borderWidth: 1.5,
+    backgroundColor: "#fff",
+    marginVertical: 0.5,
+    paddingBottom: 10,
+    paddingTop: 10,
+    paddingLeft: 10,
+    borderRadius: 10,
+  },
+  rightContainer: {
+    padding: 10,
+    width: "100%",
+    flex: 3,
+    alignItems: "flex-start",
+  },
 
-    rightContainer: {
-      padding: 10,
-      width: '100%',
-      flex: 3,
-      alignItems:'flex-start'
-    },
-    root: {
-      flexDirection: 'row',
-      borderColor: '#d1d1d1',
-      borderWidth: 1,
-      borderRadius: 10,
-      backgroundColor: '#fff',
-      marginVertical: 5,
-    }, 
-    title: {
-        fontSize: 18,
-    },
-    image: {
-      flex: 2,
-      height: 150,
-      resizeMode: 'contain',
-    },
-    price: {
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-  });
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "left",
+  },
+  titleContainer: {
 
-  export default styles;
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  userContainer: {
+    paddingTop:6,
+
+  },
+  priceContainer: {
+    top: 125,
+    right: 160,
+    position: "absolute",
+  },
+  timeContainer: {
+    top: 125,
+    left: 180,
+    position: "absolute",
+  },
+  image: {
+    borderRadius: 3,
+    right: 2.5,
+    borderColor: "gray",
+    flex: 2,
+    height: 150,
+    resizeMode: "cover",
+  },
+  time:{
+    fontSize: 18,
+    fontWeight:'bold',
+    color:'green'
+  },
+
+  price: {
+    fontSize: 18,
+  },
+});
+
+export default styles;
