5ec354b0efd79a322162c576aa95e087ca7cf897
TJ N <tui77189@temple.edu>
2022-03-29 13:06:16 -0400

    Making progress on implementing a category option for create a listing screen

---
+++ src/components/CategorySelect/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center'
+    },
+    ms: {
+        width: '80%',
+        padding: 3,
+        margin: 3,
+    },
+    result: {
+        padding: 1,
+    }
+
+});
+
+export default styles;
