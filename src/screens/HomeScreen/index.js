18ee04180ea7a93c6c0793248b49446d26162dab
Dylan <tui29835@temple.edu>
2022-04-07 10:36:07 -0400

    Testing multiple categories

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,5 +1,5 @@
 import { Auth } from "aws-amplify";
-import { Text, View, FlatList, Modal, Picker, Alert } from "react-native";
+import { Text, View, FlatList, Modal, Picker, Alert, TextComponent } from "react-native";
 import styles from "./styles";
 import ProductItem from "../../components/ProductItem";
 import React, { useEffect, useState, useContext } from "react";
@@ -15,7 +15,7 @@ import { ConsoleLogger } from "@aws-amplify/core";
 const HomeScreen = ({ searchValue }) => {
   const { client } = useChatContext();
   const [products, setProducts] = useState([]);
-  var [sortedProducts, setSortedProducts] = useState([]);
+  const [sortedProducts, setSortedProducts] = useState([]);
   const { user, setUser } = useContext(AuthContext);
   console.log(searchValue);
   
@@ -31,16 +31,212 @@ const HomeScreen = ({ searchValue }) => {
   };
   const [selectedValue, setSelectedValue] = useState("None");
 
+
+  const resetDatastore = async () => {
+    try {
+      await DataStore.stop();
+      await DataStore.start();
+      console.warn("successfully stopped, started");
+      const products = await DataStore.query(Product);
+
+   
+    } catch (e) {
+      Alert.alert("oops", e.message);
+    }
+  };
+
+  
+
+  const fetchProducts = () => {
+    // if (categories.length == 1) {
+    //   DataStore.query(Product).then(setProducts);
+    //   // setSortedProducts of categories
+    //   console.log("THIS SHOULD QUERY FOR 1 CATEGORY");
+    //   DataStore.query(Product, (product) => product.category("eq", '' + categories[0])).then(setSortedProducts);
+    // } else 
+    
+    
+    if(categories.length >= 1) {
+
+      // console.log("notCategories before querying: " + notCategories);
+
+      // removeCategory(categories[0]);
+      // removeCategory(categories[1]);
+
+      // console.log("notCategories after splicing: " + notCategories[0] + notCategories[1]);
+      // DataStore.query(Product, (product) => product.or(product => product.category("ne", notCategories[0])).category("ne" + notCategories[1])).then(setSortedProducts);
+      // console.log(sortedProducts);
+      // DataStore.query((Product), (product) => product.or(product => product.category("eq", categories[0]).category("eq", categories[1]))).then(setSortedProducts);
+
+      DataStore.query((Product), (product) => product.or(product => product
+        .category("eq", categories[0])
+        .category("eq", categories[1])
+        .category("eq", categories[2])
+        .category("eq", categories[3])
+        .category("eq", categories[4])
+        .category("eq", categories[5])
+        .category("eq", categories[6])))
+        .then(setSortedProducts);
+    } else {
+      console.log("Categories is null");
+      DataStore.query(Product).then(setProducts);
+      //console.log("products ============== ", products);
+      DataStore.query(Product).then(setSortedProducts);
+    }
+  }
+
+
+  useEffect(() => {
+    console.log("Running useEffect");
+    // resetDatastore();
+    // if(categories.length==1){
+    //   // console.log("Categories is NOT null");
+    //   console.log("Categories: " + categories);
+    //   DataStore.query(Product).then(setProducts);
+    //   // setSortedProducts of categories
+    //   console.log("THIS SHOULD QUERY FOR 1 CATEGORY");
+    //   DataStore.query(Product, (product) => product.category("eq", '' + categories[0])).then(setSortedProducts);
+    // } else 
+    
+    if (categories.length >= 1) {
+
+      console.log("notCategories before querying: " + notCategories);
+
+      removeCategory(categories[0]);
+      removeCategory(categories[1]);
+
+      console.log("notCategories after splicing: " + notCategories[0] + notCategories[1]);
+      // DataStore.query(Product, (product) => product.or(product => product.category("ne", notCategories[0])).category("ne" + notCategories[1])).then(setSortedProducts);
+      // console.log(sortedProducts);
+      DataStore.query((Product), (product) => product.or(product => product
+      .category("eq", categories[0])
+      .category("eq", categories[1])
+      .category("eq", categories[2])
+      .category("eq", categories[3])
+      .category("eq", categories[4])
+      .category("eq", categories[5])
+      .category("eq", categories[6])))
+      .then(setSortedProducts);
+    
+    } else {
+      console.log("Categories is null");
+      DataStore.query(Product).then(setProducts);
+      //console.log("products ============== ", products);
+      DataStore.query(Product).then(setSortedProducts);
+    }
+
+  }, [categories]);
+
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState([]);
+  const [items, setItems] = useState([
+    {label: 'Books', value: 'Books'},
+    {label: 'Supplies & Equipment', value: 'Supplies & Equipment'},
+    {label: 'Electronics', value: 'Electronics'},
+    {label: 'Clothes', value: 'Clothes'},
+    {label:  'Food & Nutrition', value: 'Food & Nutrition'},
+    {label: 'Handmade', value: 'Handmade'},
+    {label: 'Service', value: 'Service'},
+    {label: 'Other', value: 'Other'}
+  ]);
+  var [categories, setCategories] = useState([]);
+  var [notCategories, setNotCategories] = useState(["art", "protein", "test", "textbook"]);
+  
+
+
+  const removeCategory = (cat) => {
+
+    var array = notCategories;
+    var index = notCategories.indexOf(cat);
+    console.log("cat in removeCategory " + cat);
+    if (index !== -1) {
+      console.log("splicing " + cat);
+      array.splice(index, 1);
+      setNotCategories(array);
+    }
+
+    console.log(notCategories + "In the function!");
+  }
+
+  const applyCategories = (value) => {
+    console.log("Not Categories: " + notCategories);
+    // console.log("value in applyCategories: " + value);
+    categories=value;
+    console.log("Categories in applyCategories: " + categories);
+
+    // resetDatastore();
+
+    // if(categories.length==1){
+    //   // console.log("Categories is NOT null");
+    //   // console.log("Categories: " + categories);
+    //   // DataStore.query(Product).then(setProducts);
+    //   // setSortedProducts of categories
+    //   console.log("THIS SHOULD QUERY FOR 1 CATEGORY");
+    //   // console.log("Querying for 1 Category: " + categories[0]);
+    //   DataStore.query(Product, (product) => product.category("eq", '' + categories)).then(setSortedProducts);
+    
+
+    
+    
+    // } else 
+    
+    
+    
+    if (categories.length >= 1) {
+      // console.log("THIS SHOULD QUERY FOR 2 CATEGORIES");
+      // console.log('' + categories[0]);
+      // console.log('' + categories[1]);
+      // console.log("Querying for 2 Categories: " + categories[0] + categories[1]);
+
+      // setNotCategories(notCategories.splice(categories[0]));
+      // setNotCategories(notCategories.splice(categories[1]));
+      
+      // console.log("notCategories before querying: " + notCategories);
+
+      // removeCategory(categories[0]);
+      // removeCategory(categories[1]);
+
+      
+      // console.log("notCategories after splicing: " + notCategories[0] + notCategories[1]);
+      
+      
+      // DataStore.query((Product), (product) => product.or(product => product.category("eq", categories[0]).category("eq", categories[1]))).then(setSortedProducts);
+      
+      DataStore.query((Product), (product) => product.or(product => product
+        .category("eq", categories[0])
+        .category("eq", categories[1])
+        .category("eq", categories[2])
+        .category("eq", categories[3])
+        .category("eq", categories[4])
+        .category("eq", categories[5])
+        .category("eq", categories[6])))
+        .then(setSortedProducts);
+      // setSortedProducts(categorizedProducts);
+      // DataStore.query(Product, (product) => product.or(product => product.category("ne", notCategories[0])).category("ne" + notCategories[1])).then(setSortedProducts);
+      // console.log(sortedProducts);
+
+
+    } else {
+      console.log("Categories is null");
+      DataStore.query(Product).then(setProducts);
+      //console.log("products ============== ", products);
+      DataStore.query(Product).then(setSortedProducts);
+    }
+  }
+
+
+
   const filterSort = (sortedProducts, itemValue) => {
     setSelectedValue(itemValue);
-    console.log("Changed sort setting");
-    console.log(itemValue);
+    console.log("Changed sort setting to " + itemValue);
+    console.log("Categories in filterSort: " + categories)
 
     switch (itemValue) {
       case "none":
         console.log("No sort required");
         //No need to sort
         setSortedProducts(products);
+        applyCategories(categories);
         return;
 
       case "priceLow":
@@ -72,77 +268,10 @@ const HomeScreen = ({ searchValue }) => {
     }
   };
 
-  const resetDatastore = async () => {
-    try {
-      await DataStore.stop();
-      await DataStore.start();
-      console.warn("successfully stopped, started");
-      const products = await DataStore.query(Product);
-
-   
-    } catch (e) {
-      Alert.alert("oops", e.message);
-    }
-  };
-
-  const fetchProducts = () => {
-    if (categories.length > 0) {
-      DataStore.query(Product).then(setProducts);
-      // setSortedProducts of categories
-      DataStore.query(Product, c => c.or(c => c.category("eq", categories[0])).category("eq", categories[1]).then(setSortedProducts));
-    } else {
-      console.log("Categories is null");
-      DataStore.query(Product).then(setProducts);
-      //console.log("products ============== ", products);
-      DataStore.query(Product).then(setSortedProducts);
-    }
-  }
 
 
-  useEffect(() => {
-    console.log("Running useEffect");
-    resetDatastore();
-    if(categories.length>0){
-      console.log("Categories is NOT null");
-      console.log("Categories: " + categories);
-      DataStore.query(Product).then(setProducts);
-      // setSortedProducts of categories
-      DataStore.query(Product, c => c.or(c => c.category("eq", categories[0])).category("eq", categories[1]).then(setSortedProducts));
-    } else {
-      console.log("Categories is null");
-      DataStore.query(Product).then(setProducts);
-      //console.log("products ============== ", products);
-      DataStore.query(Product).then(setSortedProducts);
-    }
 
-  }, [categories]);
 
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState([]);
-  const [items, setItems] = useState([
-    {label: 'Protein', value: 'protein'},
-    {label: 'Test', value: 'test'}
-  ]);
-  var [categories, setCategories] = useState([])
-  const applyCategories = (value) => {
-    console.log("value in applyCategories: " + value);
-    categories=value;
-    console.log("Categories in applyCategories: " + categories);
-
-    resetDatastore();
-    if(categories.length>0){
-      console.log("Categories is NOT null");
-      console.log("Categories: " + categories);
-      DataStore.query(Product).then(setProducts);
-      // setSortedProducts of categories
-      DataStore.query(Product, c => c.or(c => c.category("eq", categories[0])).category("eq", categories[1]).then(setSortedProducts));
-    } else {
-      console.log("Categories is null");
-      DataStore.query(Product).then(setProducts);
-      //console.log("products ============== ", products);
-      DataStore.query(Product).then(setSortedProducts);
-    }
-  }
   useEffect(() => {
     // wait 2 seconds after user finishes typing to query results
     const delayDebounceFn = setTimeout(() => {
@@ -158,7 +287,8 @@ const HomeScreen = ({ searchValue }) => {
   }, [searchValue])
 
   useEffect(() => {
-    resetDatastore();
+
+    // resetDatastore();
     
   }, []);
   useEffect(() => {
@@ -199,7 +329,7 @@ const HomeScreen = ({ searchValue }) => {
               <Picker.Item label ="Date: Newest" value="dateNewest"/>
               <Picker.Item label ="Date: Oldest" value="dateOldest"/>
 
-          </Picker>
+            </Picker>
 
           <DropDownPicker
             open={open}
@@ -210,14 +340,12 @@ const HomeScreen = ({ searchValue }) => {
             setItems={setItems}
             multiple={true}
             min={0}
-            max={5}
+            max={7}
             onChangeValue={(value) => {
-              // categories = value;
-              var [temp] = value;
-              console.log("temp: " + temp);
+              setCategories(value);
               applyCategories(value);
               console.log("value: " + value);
-              console.log("Categories: " + categories);
+              console.log("Categories after setting in dropdown: " + categories);
             }}
           />
 
